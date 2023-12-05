a77ad1c253f1b9550f6dac6c639b4619a41ecedb
AnishSalvi1222 <104258169+AnishSalvi1222@users.noreply.github.com>
2023-12-05 23:45:02 +0100

    Update Subqueries.sql

--- Subqueries.sql
+++ Subqueries.sql
@@ -1,1 +1,13 @@
+## Subqueries
+
+# Write a query to find the top 5 customers with the highest total
+
+SELECT customer_id, total_spent
+FROM (
+    SELECT customer_id, SUM(order_amount) AS total_spent
+    FROM orders
+    GROUP BY customer_id
+    ORDER BY total_spent DESC
+    LIMIT 5
+) AS top_customers;
 
